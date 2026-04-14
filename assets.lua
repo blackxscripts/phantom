9a9fc7115f5eec954ab85e8c99605d0b08492e42
BLACK X SCRIPTS <blackxscripts07@gmail.com>
2026-04-14 19:53:22 -0300

    Create assets.lua

---
+++ assets.lua
@@ -0,0 +1,4 @@
+return {
+    intro = "https://raw.githubusercontent.com/blackxscripts/phantom/main/phantom-logo1.png",
+    topbar = "https://raw.githubusercontent.com/blackxscripts/phantom/main/phantom-logo2.png"
+}
